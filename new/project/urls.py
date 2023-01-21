a3dd6393137d393e36df7aa1365aa4f2f46122b0
loraccc <carolacharya1@gmail.com>
2023-01-21 20:28:20 +0545

    added EMp successfully

--- new/project/urls.py
+++ new/project/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('login',views.login_user , name='login'),
     path('',views.INDEX,name='index'),
+    path('add',views.ADD,name='add'),
 ]
--- new/project/views.py
+++ new/project/views.py
@@ -11,3 +11,22 @@ def INDEX(request):
     context={'emp':emp,}
 
     return render(request,'index.html',context)
+
+
+def ADD(request):
+    if request.method=="POST":
+        name= request.POST.get('name')
+        email= request.POST.get('email')
+        address= request.POST.get('address')
+        phone= request.POST.get('phone')
+
+        emp=Employee(
+            Name=name,     # first ko Name models wala and 2nd ma vako name html ko form vitra ko name= ""
+            Email=email,
+            Address=address,
+            Phone=phone,
+        )
+        emp.save()
+        
+    return redirect('index') # from views.py name ma vako chiz rakhni
+
